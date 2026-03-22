5148ece31774a42dcaced3c5a35725e43dc47a9c
Manoela Oliveira <166542868+manoela-oliveira@users.noreply.github.com>
2026-03-22 20:40:01 -0300

    Ajustando SistemaVeiculo

--- FiapRide/src/br/com/fiapride/main/SistemaVeiculo.java
+++ FiapRide/src/br/com/fiapride/main/SistemaVeiculo.java
@@ -7,12 +7,12 @@ public class SistemaVeiculo {
     public static void main(String[] args) {
         System.out.println("--- Sistema FiapRide ---");
 
-        Veiculo veiculo1 = new Veiculo("Ana Silva","BRA2E19");
-        System.out.println("\n>>>> Abastecimento do veículo do proprietário" + veiculo1.getProprietario() +".");
+        Veiculo veiculo1 = new Veiculo("Carlos","ABC-123");
+        System.out.println("\n>>>> Abastecimento do veículo do proprietário " + veiculo1.getProprietario() +".");
         veiculo1.adicionarGasolina(50.0);
         veiculo1.consumirGasolina(100);
 
-        System.out.println("\n>>>> Status do veículo do proprietário" + veiculo1.getProprietario() +".");
-        System.out.println("Dono: " + veiculo1.getProprietario() + " | Placa: " + veiculo1.getPlaca() + " | Gasolina: " + veiculo1.getGasolina());
+        System.out.println("\n>>>> Status do veículo:");
+        System.out.println("Proprietário: " + veiculo1.getProprietario() + " | Placa: " + veiculo1.getPlaca() + " | Gasolina: " + veiculo1.getGasolina() + "l");
     }
 }
