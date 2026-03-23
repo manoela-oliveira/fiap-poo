8bacc38e41042a6e91dc24765aaa931575b5fe3a
manoela-oliveira <manoela.oliveira0900@gmail.com>
2026-03-23 20:27:12 -0300

    Adicionando comentários e getter gasolina

--- FiapRide/src/br/com/fiapride/main/SistemaVeiculo.java
+++ FiapRide/src/br/com/fiapride/main/SistemaVeiculo.java
@@ -1,12 +1,16 @@
 package br.com.fiapride.main;
 
 import br.com.fiapride.model.Veiculo;
-
+// Alterando nomeclatura da classe a fim de facilitar identificação
 public class SistemaVeiculo {
     
     public static void main(String[] args) {
         System.out.println("--- Sistema FiapRide ---");
-
+        /* Instânciando objeto utilizando sistema de atributos do arquivo Veiculo.java e utilizando
+           "new" para alocação de memória
+           Utilizando nomeclatura significativa para  objeto
+           De: v1
+           Para: veiculo1 */
         Veiculo veiculo1 = new Veiculo("Carlos","ABC-123");
         System.out.println("\n>>>> Abastecimento do veículo do proprietário " + veiculo1.getProprietario() +".");
         veiculo1.adicionarGasolina(50.0);
--- FiapRide/src/br/com/fiapride/model/Veiculo.java
+++ FiapRide/src/br/com/fiapride/model/Veiculo.java
@@ -1,5 +1,10 @@
 package br.com.fiapride.model;
 
+/* Alterando nomeclaturas antigas para nomeclaturas significativas
+   De: individuo, pl e gas
+   Para: proprietário, placa e gasolina
+   Transformando atributos que estavam públicos
+   em arquivos privados */
 public class Veiculo {
     private String proprietario;
     private String placa;
@@ -8,9 +13,11 @@ public class Veiculo {
     public Veiculo(String proprietario, String placa) {
         this.setProprietario(proprietario);
 		this.setPlaca(placa);
-		this.gasolina = 0;
+		this.setGasolina(0.0);
     }
-
+    /* Aplicando padrão de nomeclatura getter"CamelCase e tornando-o público
+       De: private get_individuo
+       Para: public String getProprietario */
 	public String getProprietario() {
 		return this.proprietario;
 	}
@@ -38,9 +45,21 @@ public class Veiculo {
 	}
     
     public double getGasolina() {
-        return gasolina;
+        return this.gasolina;
     }
-
+    /* Utilizando Setter para o atributo "gasolina" a fim de não permitir valores negativos 
+       em hipótese alguma, conforme demonstrado no código legado v1.gas = -10; e tornando-o privado */
+    private void setGasolina(double valor) {
+        if (valor >= 0){
+            this.gasolina = valor;
+        }
+        else {
+            System.out.println("Erro: Tentativa de definir gasolina negativa negativo bloqueada!");
+        }
+    }
+    /* Alteração do tipo, visto que gasolina é medida em litros
+       De: int (permite apenas números inteiros)
+       Para: double (permite números decimais) */
     public void adicionarGasolina(double valor) {
         if (valor <= 0) {
             System.out.println("Erro: Valor de reabastecimento deve ser superior a zero.");
@@ -51,7 +70,9 @@ public class Veiculo {
         this.gasolina += valor;
         System.out.println("Abastecimento realizado com sucesso! Você possui agora " + this.gasolina + " litros de gasolina em seu tanque.");
     }
-    
+    /* Transformando ações em verbos e transformando nomeclatura anterior em uma nomeclatura significativa
+       De: gasta
+       Para: consumirGasolina */
     public void consumirGasolina(double gasto) {
         if (gasto <= 0) {
             System.out.println("Erro: Valor inválido. O valor consumo de gasolina deve ser superior a zero.");
